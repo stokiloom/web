bdff8b581a787598e288507eef37ab2568860d9f
stokiloom <stokiloom@gmail.com>
2020-08-06 14:20:29 +0000

    test4

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 def myapp(environ, start_response):
     status = '200 OK'
     headers = [('Content-Type', 'text/plain')]
-    body = [p + '\n' for p in environ['QUERY_STRING'].decode('ascii').split('&')]
+    body = [bytes(p + '\n', 'ascii') for p in environ['QUERY_STRING'].split('&')]
     start_response(status, headers)
     return body
 
